99260839a3c360b7a8fab7da337d50ad22881017
Mbida Stephan <mbida@medicasure.com>
2023-03-28 13:42:54 +0100

    changement de police d'écritureé

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default{
 @import url('https://fonts.googleapis.com/css?family=Exo+2');
 #app {
   background-color: #F8F8F8 !important;
-  font-family: 'Exo 2', sans-serif !important;
+  font-family: 'montserrat', sans-serif !important;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
