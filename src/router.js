9d6c67d9ad12cb6af3fda894fc0640e55b02a6d4
Mbida Stephan <mbida@medicasure.com>
2023-05-16 10:37:43 +0100

    ajout de l'importation des data

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-const Home = () => import('@/views/Home.vue');
-const Products = () => import('@/views/Products.vue');
-const Contact = () => import('@/views/Contact.vue');
-const Info = () => import('@/views/Info.vue');
+// const Home = () => import('@/views/Home.vue');
+// const Products = () => import('@/views/Products.vue');
+// const Contact = () => import('@/views/Contact.vue');
+// const Info = () => import('@/views/Info.vue');
+
+import Home from '@/views/Home.vue';
+import Products from '@/views/Products.vue';
+import Contact from '@/views/Contact.vue';
+import Info from '@/views/Info.vue';
 
 
 Vue.use(Router);
